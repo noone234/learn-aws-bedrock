ee5cfe89597f282cd07737b760bdff67a16aea5c
Christopher Wolfe <noone234@gmail.com>
2024-04-20 07:33:41 -0400

    chore: CDK IaC for a Lambda function

--- infrastructure/service_stack.py
+++ infrastructure/service_stack.py
@@ -1,19 +1,23 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda,
+    aws_sqs as sqs,
 )
+from aws_cdk.aws_lambda_python_alpha import PythonFunction
 from constructs import Construct
 
-class LearnAwsBedrockStack(Stack):
+
+class ServiceStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "LearnAwsBedrockQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        PythonFunction(
+            self,
+            "LambdaFunction",
+            runtime=aws_lambda.Runtime.PYTHON_3_12,
+            entry="./app/lambdas",
+            index="example2.py",
+            handler="lambda_handler",
+        )
