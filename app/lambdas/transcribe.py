05b7d567ddcd30726df700629f79ff87919189fb
Christopher Wolfe <noone234@gmail.com>
2024-05-05 20:09:55 -0400

    chore: infer audio format from file extension

--- app/lambdas/transcribe.py
+++ app/lambdas/transcribe.py
@@ -8,19 +8,20 @@ transcribe_client = boto3.client("transcribe", region_name="us-west-2")
 
 
 def lambda_handler(event, context):
-    # Extract the bucket name and key from the incoming event
+    # Extract bucket and file name from incoming event.
     bucket = event["Records"][0]["s3"]["bucket"]["name"]
     key = event["Records"][0]["s3"]["object"]["key"]
 
-    # One of a few different checks to ensure we don't end up in a recursive loop.
-    try:
+    # Infer audio format from input file extension.
+    audio_format = key.split(".")[-1]
 
+    try:
         job_name = "transcription-job-" + str(uuid.uuid4())  # Needs to be a unique name
 
-        response = transcribe_client.start_transcription_job(
+        transcribe_client.start_transcription_job(
             TranscriptionJobName=job_name,
             Media={"MediaFileUri": f"s3://{bucket}/{key}"},
-            MediaFormat="mp3",
+            MediaFormat=audio_format
             LanguageCode="en-US",
             OutputBucketName=os.environ[
                 "S3BUCKETNAMETEXT"
